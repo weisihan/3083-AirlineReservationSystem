29fe3f185386dab59bad907ff760145fdbbd7a0a
silvia.wei@nyu.edu <silvia.wei@nyu.edu>
2022-04-13 10:50:46 -0400

    updated login
    connected to json fake database

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 
 function Login() {
-  const [details, setDetails] = useState({
-    fname: "",
-    lname: "",
-    email: "",
-    password: "",
-  });
+  // const [details, setDetails] = useState({
+  //   fname: "",
+  //   lname: "",
+  //   email: "",
+  //   password: "",
+  // });
 
   return (
     <div className="card">
-      <h2>Choose to register as a client or staff</h2>
+      <h2>Choose to login as a client or staff</h2>
     </div>
   );
 }
