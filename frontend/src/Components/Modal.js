bf56f99b902248f0423eb2bd5b0fec61fe98a445
silvia.wei@nyu.edu <silvia.wei@nyu.edu>
2022-04-13 00:44:00 -0400

    cleanup

--- frontend/src/Components/Modal.js
+++
@@ -1,8 +0,0 @@
-function Modal(props) {
-    return (<div className="modal">
-        <p>Incorrect username or password</p>
-        <button className="btn"  onClick ={props.onCancel}>Cancel</button>
-        </div>);
-}
-
-export default Modal
