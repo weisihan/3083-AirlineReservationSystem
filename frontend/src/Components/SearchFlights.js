502fbcd558d187858dd276a540661b4b411c8d0b
Silvia Wei <silvia.wei@nyu.edu>
2022-04-13 14:07:29 +0800

    added date selector (#5)
    * added date selector

--- frontend/src/Components/SearchFlights.js
+++ frontend/src/Components/SearchFlights.js
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
 
 function SearchFlights() {
   return (
     <div className="card">
       <h1>Search for flights </h1>
       <div className="actions">
+        <Stack component="form" noValidate spacing={3}>
+          <TextField
+            id="date"
+            label="Choose a date"
+            type="date"
+            defaultValue="2022-05-05"
+            sx={{ width: 220 }}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </Stack>
+        <br></br>
         <Link to="/clienthome">
           <button className="btn">Go to client home</button>
         </Link>
