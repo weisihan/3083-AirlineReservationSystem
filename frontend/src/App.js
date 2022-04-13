a666315e9dee00fe25601f765d6e7ea95a18577b
Silvia Wei <silvia.wei@nyu.edu>
2022-04-13 13:59:20 +0800

    fixed bugs (#4)
    * Implemented homepage login register
    
    * updated login
    
    * cleanup
    
    * implemented staff home
    
    * implemented staff home
    
    * fixed bugs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -57,6 +57,13 @@ function LoginLayout() {
           <Link to="/stafflog">
             <button className="btn"> Staff </button>
 
+          </Link>
+        </Stack>
+      </nav>
+      <div className="content"></div>
+      <Outlet />
+    </div>
+  );
 }
 function App() {
   return (
@@ -65,7 +72,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/clienthome" element={<ClientHome />} />
         <Route path="/staffhome" element={<StaffHome />} />
-          
+
         <Route path="/myflights" element={<MyFlights />} />
         <Route path="/searchflights" element={<SearchFlights />} />
         <Route path="/purchaseticket" element={<PurchaseTicket />} />
@@ -79,7 +86,20 @@ function App() {
         <Route path="/newairport" element={<NewAirport />} />
         <Route path="/viewfeedback" element={<ViewFeedback />} />
         <Route path="/viewreport" element={<ViewReport />} />
-)
+
+        <Route path="/" element={<RegisterLayout />}>
+          <Route path="/register" element={<Register />} />
+          <Route path="/staffreg" element={<StaffReg />} />
+          <Route path="/clientreg" element={<ClientReg />} />
+        </Route>
+        <Route path="/" element={<LoginLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/stafflog" element={<StaffLog />} />
+          <Route path="/clientlog" element={<ClientLog />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
