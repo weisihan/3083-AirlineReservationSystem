1286acf8042bebf7e5cbaa3f86a467ea53ae81a5
Silvia Wei <silvia.wei@nyu.edu>
2022-04-15 11:37:14 +0800

    updated date picker (#12)
    * Implemented homepage login register
    
    * updated login
    
    * cleanup
    
    * implemented staff home
    
    * implemented staff home
    
    * fixed bugs
    
    * added date selector
    
    * updated login
    
    connected to json fake database
    
    * updated register
    
    work in progress...
    
    * updated registration
    
    connected to fake database
    
    * implemented add flight and add airport
    
    * tiny update
    
    * Update index.js
    
    * updated date picker

--- frontend/src/Components/SearchFlights.js
+++ frontend/src/Components/SearchFlights.js
@@ -3,23 +3,20 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
+import DateFnsUtils from "@date-io/date-fns"; // choose your lib
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
 
 function SearchFlights() {
+  const [selectedDate, handleDateChange] = useState(new Date());
+
   return (
     <div className="card">
       <h1>Search for flights </h1>
       <div className="actions">
         <Stack component="form" noValidate spacing={3}>
-          <TextField
-            id="date"
-            label="Choose a date"
-            type="date"
-            defaultValue="2022-05-05"
-            sx={{ width: 220 }}
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <DatePicker value={selectedDate} onChange={handleDateChange} />
+          </MuiPickersUtilsProvider>
         </Stack>
         <br></br>
         <Link to="/clienthome">
