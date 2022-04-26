63521c322d135beb527001bac9bb12907383755c
Silvia Wei <silvia.wei@nyu.edu>
2022-04-26 11:01:52 -0400

    Silvia branch (#16)
    * Implemented homepage login register
    
    * updated login
    
    * cleanup
    
    * implemented staff home
    
    * implemented staff home
    
    * fixed bugs
    
    * added date selector
    
    * updated login
    
    connected to json fake database
    
    * updated register
    
    work in progress...
    
    * updated registration
    
    connected to fake database
    
    * implemented add flight and add airport
    
    * tiny update
    
    * Update index.js
    
    * updated date picker
    
    * starting authentication, fixed minor details
    
    * authentication work in progress
    
    * authentication done
    
    authentication done

--- frontend/src/Components/ClientHome.js
+++ frontend/src/Components/ClientHome.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 // async function getUser() {
 //   const resBody = await axios.post("http://localhost:3001/clienthome");
@@ -11,62 +12,65 @@ import axios from "axios";
 //   }
 // }
 
-async function success() {}
-
 function ClientHome() {
+  let navigate = useNavigate();
   const [details, setDetails] = useState({
     status: "",
   });
 
   async function getStatus() {
     const resBody = await axios.post("http://localhost:3001/clientcheck");
-    if (resBody.data === true) {
+
+    const res = resBody.data;
+    if (res === true) {
       console.log("logged in");
       details.status = "true";
     } else {
       alert("you are not logged in");
+      console.log("navigating to home");
+      navigate("/home");
       details.status = "false";
     }
   }
-  getStatus();
-  console.log("status is", details.status);
 
-  if (details.status === "true") {
-    return (
-      <div className="card">
-        <h1>Client Homepage </h1>
-        <h2>Welcome to client homepage</h2>
-        <div className="actions">
-          <Link to="/myflights">
-            <button className="btn">View my flights</button>
-          </Link>
-          <Link to="/searchflights">
-            <button className="btn">Search for flights</button>
-          </Link>
-          <br></br>
-          <br></br>
-          <Link to="/purchaseticket">
-            <button className="btn">Purchase ticket</button>
-          </Link>
-          <Link to="/review">
-            <button className="btn">Review</button>
-          </Link>
-          <br></br>
-          <br></br>
-          <Link to="/trackspending">
-            <button className="btn">Track spending</button>
-          </Link>
-          <Link to="/logout">
-            <button className="btn--alt"> Logout </button>
-          </Link>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <Link to="/home">
-          <button className="btn">Return to home</button>
+  useEffect(() => {
+    if (details.status === "") {
+      getStatus();
+    }
+  });
+
+  useEffect(() => {
+    if (details.status === "false") {
+      navigate("/home");
+    }
+  });
+
+  return (
+    <div className="card">
+      <h1>Client Homepage </h1>
+      <h2>Welcome to client homepage</h2>
+      <div className="actions">
+        <Link to="/myflights">
+          <button className="btn">View my flights</button>
+        </Link>
+        <Link to="/searchflights">
+          <button className="btn">Search for flights</button>
+        </Link>
+        <br></br>
+        <br></br>
+        <Link to="/purchaseticket">
+          <button className="btn">Purchase ticket</button>
+        </Link>
+        <Link to="/review">
+          <button className="btn">Review</button>
+        </Link>
+        <br></br>
+        <br></br>
+        <Link to="/trackspending">
+          <button className="btn">Track spending</button>
+        </Link>
+        <Link to="/logout">
+          <button className="btn--alt"> Logout </button>
         </Link>
       </div>
     );
