47da32d700e29b10a32eefeee3bd498b4e59f06a
silvia.wei@nyu.edu <silvia.wei@nyu.edu>
2022-05-03 22:08:20 -0400

    home page done

--- frontend/src/Router/Home/Home.js
+++ frontend/src/Router/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import Table from "../../Components/Table.component";
 
 async function AllFlights() {
   const [details, setDetails] = useState({
@@ -12,12 +13,40 @@ async function AllFlights() {
 }
 
 function Home() {
-  const [flightData, setFlightData] = useState(null);
+  const [flightData, setFlightData] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       let flights = await axios.get("http://localhost:3001/home");
-      setFlightData(flights.data);
+      flights = flights.data;
+      console.log(flights);
+      // airline_name: "Air China";
+      // airplane_airline_name: "Air China";
+      // airplane_id: "3";
+      // arr_airport: "LAX";
+      // arr_date: "2021-11-12T05:00:00.000Z";
+      // arr_time: "16:50:25";
+      // base_price: 300;
+      // dept_airport: "SFO";
+      // dept_date: "2021-11-12T05:00:00.000Z";
+      // dept_time: "13:25:25";
+      // flight_num: "102";
+      // flight_status: "On-time";
+      let results = [];
+      for (let i = 0; i < flights.length; i++) {
+        let temp = [];
+        let current = flights[i];
+        temp = [
+          current.flight_num,
+          current.airline_name,
+          current.dept_airport,
+          current.arr_airport,
+        ];
+        results.push(temp);
+      }
+
+      setFlightData(results);
+
       console.log(flights);
     }
     fetchData();
@@ -26,28 +55,12 @@ function Home() {
 
   console.log(flightData);
 
+  let heading = ["Flight Number", "Company Name", "Departure", "Arrival"];
+
   return (
     <div className="card">
       <h1>Homepage </h1>
       <h2>Welcome to xxx airlines</h2>
-      {flightData ? (
-        flightData.map((flight) => {
-          return (
-
-            <DataGrid
-  onCellClick={(params: GridCellParams, event: MuiEvent<React.MouseEvent>) => {
-    event.defaultMuiPrevented = true;
-  }}
-/>
-            <p>
-              Airline: {flight.airline_name} Flight Number:
-              {flight.flight_num}
-            </p>
-          );
-        })
-      ) : (
-        <p>loading..</p>
-      )}
       <div>
         <div>
           <h2>{"Please sign in or register"} </h2>
@@ -62,6 +75,7 @@ function Home() {
                 </Link>
               </div>
               <br></br>
+              <Table heading={heading} body={flightData} />
             </div>
           </div>
         </div>
