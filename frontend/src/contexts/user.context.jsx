c2fc6ce8ed6c68f94c19e5110e19904a3e579d5f
silvia.wei@nyu.edu <silvia.wei@nyu.edu>
2022-05-04 01:56:46 -0400

    Update user.context.jsx

--- frontend/src/contexts/user.context.jsx
+++ frontend/src/contexts/user.context.jsx
@@ -5,7 +5,7 @@ export const UserContext = createContext({
     setCurrentUser: () => null,
 });
 export const UserProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState("rootuser@gmail.com");
+    const [currentUser, setCurrentUser] = useState(null);
     const value = {currentUser, setCurrentUser};
     return <UserContext.Provider value = {value}>{children}</UserContext.Provider>
 };
