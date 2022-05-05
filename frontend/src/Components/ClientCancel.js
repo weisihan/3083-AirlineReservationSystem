45e602fe03953456e9cbfc897c1ac6c9c9c3537b
olivia lin <68142008+oliviafroglin@users.noreply.github.com>
2022-05-05 00:24:59 -0400

    Client Cancel Flight Completed!!!
    YAYYYYYYY

--- frontend/src/Components/ClientCancel.js
+++ frontend/src/Components/ClientCancel.js
@@ -2,11 +2,12 @@ import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { UserContext } from "../contexts/user.context";
+import Table from "./Table.component";
 
 function Cancel() {
   const { currentUser } = useContext(UserContext);
-  console.log(currentUser);
   const [formDetails, setDetails] = useState({
+    ticketId: "",
     airlineName: "",
     flightNum: "",
     depDate: "",
@@ -20,14 +21,41 @@ function Cancel() {
       formDetails
     );
   }
+  async function getId(formDetails) {
+    let res = await axios.post("http://localhost:3001/ticketInfo", formDetails);
+    // console.log("ticket", res);
+    res = res.data;
+    console.log("ticketdata", res);
+    let aggregated = [];
+    for (let i = 0; i < res.length; i++) {
+      //   console.log(res[i].ticketId);
+      console.log(res[i].ticket_id);
+      aggregated.push([res[i].email, res[i].ticket_id]);
+    }
+    console.log("agg", aggregated);
+    //here
+    setFlights(aggregated);
+  }
   function handleEvent(event) {
     console.log("targetname", event.target.name);
     setDetails({ ...formDetails, [event.target.name]: event.target.value });
     console.log(formDetails);
   }
+  const [foundFlights, setFlights] = useState([]);
+  let heading = ["Your Email", "Your Ticket Ids"];
+
   return (
     <div className="card">
       <h1>Cancel Flight </h1>
+      <button className="btn" onClick={() => getId(formDetails)}>
+        {" "}
+        Display Your Ticket IDs{" "}
+      </button>
+      <Table heading={heading} body={foundFlights} />
+      <div className="form-group">
+        <label> Ticket Id </label>
+        <input type="text" name="ticketId" onChange={(e) => handleEvent(e)} />
+      </div>
       <div className="form-group">
         <label> Airline Name </label>
         <input
@@ -36,6 +64,7 @@ function Cancel() {
           onChange={(e) => handleEvent(e)}
         />
       </div>
+
       <div className="form-group">
         <label> Departure Date </label>
         <input type="text" name="depDate" onChange={(e) => handleEvent(e)} />
@@ -45,10 +74,12 @@ function Cancel() {
         <label> Departure Time </label>
         <input type="text" name="depTime" onChange={(e) => handleEvent(e)} />
       </div>
+
       <div className="form-group">
         <label> Flight Number </label>
         <input type="text" name="flightNum" onChange={(e) => handleEvent(e)} />
       </div>
+
       <button className="btn" onClick={() => sendRequest(formDetails)}>
         {" "}
         Confirm Cancel{" "}
